60cd1ba87ba87de36fd547491558b41459217531
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2026-06-09 11:03:09 +0530

    Add WebConfig

---
+++ src/main/java/com/fd/management/backend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.fd.management.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://178.128.218.68", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
